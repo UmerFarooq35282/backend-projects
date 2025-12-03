d9f23b27bfa5e2a2ecdbf188d94d5051154375e3
UmerFarooq35282 <umerfarooq2821035@gmail.com>
2025-12-04 00:58:35 +0500

    added cors policy for localhost and one deployed frontend investmentAGent frontend

---
+++ api/config/origin.config.js
@@ -0,0 +1,4 @@
+export const allowedOrigins = [
+    "http://localhost:5173",
+    "https://fanciful-cucurucho-f7b801.netlify.app/"
+]
--- api/index.js
+++ api/index.js
@@ -4,10 +4,24 @@ const dotenv = require("dotenv");
 const { connectDB } = require("./config/db.config.js");
 const { projectRoutes } = require("./projetcs/portfolio/routes");
 const { responseRouter } = require("./projetcs/genAI(Invesment)/routes/index.js");
+const cors = require("cors");
+const { allowedOrigins } = require("./config/origin.config.js");
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
